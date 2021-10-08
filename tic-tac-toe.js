932a945ab3cd02fcc08623f3159cb458ba4ea3b9
Marlon Lewis <marlonlewis483@gmail.com>
2021-10-08 05:35:20 -0500

    Modified js file to allow hover, click, player alternation and tile formatting for each player using the class attribute.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,52 @@
 window.onload = function(){
     var gameboard = document.getElementById("board");
     var board_tiles = gameboard.getElementsByTagName("div");
+    let plays = 0;
+    const new_game = document.getElementsByClassName("btn");
+    const players = ['X','O'];
     
     for (var tile = 0; tile < board_tiles.length; tile++){
-        addClass(board_tiles[tile],"square")
+        let ex = board_tiles[tile];
+        addClass(ex,"square");
+        ex.addEventListener("mouseover", function(){
+            addClass(ex,"hover");
+        })
+        ex.addEventListener("mouseout", function(){
+            rmvClass(ex,"hover");
+        })
+        ex.addEventListener("click", function(){
+            mark_play(ex);
+        })
     }
 
-    /*Adds class element to element*/
+    new_game[0].addEventListener("click", function(){
+        clear();
+    })
+
+    /* Adds class attribute to element */
     function addClass(ele, newclass){
         ele.classList.add(newclass);
     }
 
-    
+    /*Removes list of classes attribute to element*/
+    function rmvClass(ele, ...rmvlist){
+        ele.classList.remove(...rmvlist);
+    }
+
+   function mark_play(ele){
+       var player = players[plays%2];
+       ele.innerHTML = player;
+       ele.style.pointerEvents = 'none';
+       addClass(ele, player);
+       player = players[++plays%2];
+   }
+
+   function clear(){
+       for (i=0;i<board_tiles.length;i++){
+           rmvClass(board_tiles[i], "X", "O");
+           board_tiles[i].innerHTML = "";
+           board_tiles[i].style.pointerEvents = "auto";
+       }
+    }
+
 }
