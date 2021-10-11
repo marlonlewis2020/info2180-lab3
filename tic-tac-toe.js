fc7f134ec17b43a22884614ed3d78ed45f66c8ed
Marlon Lewis <marlonlewis483@gmail.com>
2021-10-11 03:22:03 -0500

    refacored and cleaned up code in js file.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,35 +1,27 @@
 window.onload = function(){
-    let game_number = 1;
-    let winner = "";
-    let wins = [0,0];
-    let plays = 0;
-    var player = "X";
+    let game_number = 1; plays = 0; wins = [0,0]; winner = ""; 
+    let player = "X";
     const game = document.getElementById("game");
     const status = document.getElementById("status");
     const gameboard = document.getElementById("board");
     const board_tiles = gameboard.getElementsByTagName("div");
-    // const controls = document.getElementsByClassName("controls")[0];
     const new_game = document.getElementsByClassName("btn");
     const players = ["X","O"];
+
     const para = document.createElement("div");
     para.innerHTML = "Let's Begin, Player '"+player+"'!";
+    let score_table = document.createElement("table");
+    let tb_row1 = document.createElement("tr"); 
+    let tb_row2 = document.createElement("tr");
+    let x_head = document.createElement("td"); 
+    let o_head = document.createElement("td");
+    
     let caption = document.createElement("caption");
-    let x_score = document.createElement("td");
+    let x_score = document.createElement("td"); 
     let o_score = document.createElement("td");
-    x_score.innerHTML = wins[0];
-    o_score.innerHTML = wins[1]; 
-    style_xo();
-    points_table();
-
-    game.insertBefore(para, gameboard);
-    new_game[0].style.margin = "10px 80px";
-    para.style.marginBottom = "15px";
-    para.style.backgroundColor = "#cbd5e0";
-    para.style.fontSize = "24px";
-    para.style.width = "250px";
-    para.style.fontWeight = "bold";
-    para.style.textAlign = "center";
-    para.style.borderRadius = ".25rem";
+    x_score.innerHTML = wins[0]; o_score.innerHTML = wins[1];
+     
+    style_xo(); points_table(); newElements_Styles();
 
     for (var tile = 0; tile < board_tiles.length; tile++){
         let ex = board_tiles[tile];
@@ -39,14 +31,7 @@ window.onload = function(){
         ex.addEventListener("click", function(){mark_play(ex)})
     }
 
-    new_game[0].addEventListener("click", function(){
-        clear();
-    })
-
-    function points(){
-        let points = "Move your mouse over a square and click to play an X or an O.";
-        return points;
-    }
+    new_game[0].addEventListener("click", function(){clear()})
 
     function checkWinner(){
         var pops = [[0,1,2],[3,4,5],[6,7,8], [0,4,8],[2,4,6], [0,3,6],[1,4,7],[2,5,8]];
@@ -67,7 +52,6 @@ window.onload = function(){
         if (winner=="X"){wins[0]++}
         else if (winner =="O"){ wins[1]++}
         status.innerHTML = "Congratulations! " + winner + " is the Winner!";
-        console.log(status.innerHTML);
         status.classList.add("you-won");
         lock();
         update_points();
@@ -86,25 +70,56 @@ window.onload = function(){
     }
 
     function points_table(){
-        let score_table = document.createElement("table");
-        game.insertBefore(score_table,gameboard);
         caption.innerHTML = "Game # "+game_number+"! Match Points";
-        score_table.appendChild(caption);
-
-        let tb_row1 = document.createElement("tr"); //creates header row
-        let x_head = document.createElement("td"); //creates X header cell
-        let o_head = document.createElement("td"); //creates O header cell
         x_head.innerHTML = players[0]; //sets the X header data
         o_head.innerHTML = players[1]; //sets the O header data
-        tb_row1.appendChild(x_head); //places the X header cell into row
-        tb_row1.appendChild(o_head); //places the O cell header into row
-        score_table.appendChild(tb_row1);
+        tb_row1.append(x_head,o_head); //places the X and O header cells into row 1
+        tb_row2.append(x_score,o_score); //places the X and O data cells into row 2
+        score_table.append(caption,tb_row1,tb_row2);
+    }
+    
+    function mark_play(ele){
+       player = players[plays%2];
+       ele.innerHTML = player;
+       ele.style.pointerEvents = 'none';
+       ele.classList.add(player);
+       checkWinner();
+       player = players[++plays%2];
+       style_xo();
+       turn_tracker();
+   }
 
-        let tb_row2 = document.createElement("tr");
+   function turn_tracker(){para.innerHTML = "It Is Player "+player+"'s Turn!"}
+   
+   function clear(){
+       caption.innerHTML = "Game # "+game_number+"! Match Points";
+       status.classList.remove("you-won");
+       winner = "";
+       status.innerHTML = "Move your mouse over a square and click to play an X or an O.";
+       for (i=0;i<board_tiles.length;i++){
+           board_tiles[i].classList.remove(...players);
+           board_tiles[i].innerHTML = "";
+           board_tiles[i].style.pointerEvents = "auto";
+       }
+    }
+
+    function lock(){
+        for (i=0;i<9;i++){board_tiles[i].style.pointerEvents = 'none'}
+    }
+
+    function newElements_Styles(){
+        game.insertBefore(score_table,gameboard);
+        game.insertBefore(para, gameboard);
+
+        new_game[0].style.margin = "10px 80px";
         
-        tb_row2.appendChild(x_score);
-        tb_row2.appendChild(o_score);
-        score_table.appendChild(tb_row2);
+        para.style.marginBottom = "15px";
+        para.style.backgroundColor = "#cbd5e0";
+        para.style.fontSize = "24px";
+        para.style.width = "250px";
+        para.style.fontWeight = "bold";
+        para.style.textAlign = "center";
+        para.style.borderRadius = ".25rem";
 
         caption.style.fontWeight = "bold";
         caption.style.color = "gray";
@@ -117,39 +132,6 @@ window.onload = function(){
         tb_row2.style.color = "gray";
         score_table.style.width = "300px";
         score_table.style.textAlign = "center";
-    }
-
-   function mark_play(ele){
-       player = players[plays%2];
-       ele.innerHTML = player;
-       ele.style.pointerEvents = 'none';
-       ele.classList.add(player);
-       checkWinner();
-       player = players[++plays%2];
-       style_xo();
-       turn_tracker();
-   }
-
-   function turn_tracker(){
-    para.innerHTML = "It Is Player "+player+"'s Turn!";
-   }
-
-   function clear(){
-       caption.innerHTML = "Game # "+game_number+"! Match Points";
-       status.classList.remove("you-won");
-       winner = "";
-       status.innerHTML = points();
-       for (i=0;i<board_tiles.length;i++){
-           board_tiles[i].classList.remove(...players);
-           board_tiles[i].innerHTML = "";
-           board_tiles[i].style.pointerEvents = "auto";
-       }
-    }
-
-    function lock(){
-        for (i=0;i<9;i++){
-            board_tiles[i].style.pointerEvents = 'none';
-        }
     }
 
 }
